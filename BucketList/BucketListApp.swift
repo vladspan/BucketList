2ccbea5b189abb87d0a8cf1bec3c7428433e6193
vladspan <vladimir.spanic01@gmail.com>
2023-11-09 08:15:29 +0100

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Vladimir Spanic on 09.11.2023..
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
