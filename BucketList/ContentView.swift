7ad63bf5d301b02d50de4064bf52403bacd325f3
Vladimir Spanic <vladimir.spanic01@gmail.com>
2023-11-09 09:22:14 +0100

    Initial commit

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,16 +6,44 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            Map(coordinateRegion: $mapRegion)
+                .ignoresSafeArea()
+            
+            Circle()
+                .fill(.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            
+            VStack {
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    
+                    Button {
+                        // create a new location
+                    } label: {
+                        Image(systemName: "plus")
+                           
+                    }
+                    .padding()
+                    .background(.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                }
+            }
+            }
+        
+        
     }
 }
 
