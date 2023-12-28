00ce409cf6e4fcc10a9344a387ab71cbe4a258cb
vladspan <117947562+vladspan@users.noreply.github.com>
2023-12-28 15:48:12 +0100

    Add files via upload

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -2,18 +2,40 @@
 //  ContentView.swift
 //  BucketList
 //
-//  Created by Vladimir Spanic on 09.11.2023..
+//  Created by Vladimir Spanic on 13.11.2023..
 //
 
-import SwiftUI
 import MapKit
+import SwiftUI
 
 struct ContentView: View {
     @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
     
+    @State private var locations = [Location]()
+    
+    @State private var selecetdPlace: Location?
+    
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $mapRegion)
+            Map(coordinateRegion: $mapRegion, annotationItems: locations){ location in
+                MapAnnotation(coordinate: location.coordinate ){
+                    VStack{
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundColor(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        
+                        Text(location.name)
+                            .fixedSize()
+                        
+                    }
+                    .onTapGesture {
+                        selecetdPlace = location
+                    }
+                }
+            }
                 .ignoresSafeArea()
             
             Circle()
@@ -21,29 +43,33 @@ struct ContentView: View {
                 .opacity(0.3)
                 .frame(width: 32, height: 32)
             
-            VStack {
+            VStack{
                 Spacer()
-                
                 HStack{
                     Spacer()
-                    
-                    Button {
-                        // create a new location
+                    Button{
+                        let newLocation = Location(id: UUID(), name: "New location", descripiton: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+                        locations.append(newLocation)
                     } label: {
                         Image(systemName: "plus")
-                           
                     }
                     .padding()
                     .background(.black.opacity(0.75))
                     .foregroundColor(.white)
                     .font(.title)
-                    .clipShape(Circle())
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                     .padding(.trailing)
                 }
             }
+        }
+        .sheet(item: $selecetdPlace){ place in
+            EditView(location: place){ newLocation in
+                if let index = locations.firstIndex(of: place) {
+                    locations[index] = newLocation
+                }
+                
             }
-        
-        
+        }
     }
 }
 
